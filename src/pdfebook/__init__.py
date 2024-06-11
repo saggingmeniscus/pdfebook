483ccfada4e705742f3ed801437664d6cd0a57ab
Jacob Smullyan <smulloni@smullyan.org>
2024-06-11 13:39:49 -0400

    better help text

--- src/pdfebook/__init__.py
+++ src/pdfebook/__init__.py
@@ -28,12 +28,44 @@ def get_cover_pages(size, cover):
 
 
 @click.command()
-@click.option("-t", "--title", required=True)
-@click.option("-a", "--author", required=True)
-@click.option("-s", "--size", default="A5")
-@click.option("-c", "--cover", required=True, type=click.Path(exists=True))
-@click.option("-i", "--interior", required=True, type=click.Path(exists=True))
-@click.option("-o", "--outfile", required=True, type=click.Path())
+@click.option("-t", "--title", required=True, help="Title of book")
+@click.option(
+    "-a",
+    "--author",
+    required=True,
+    help="Author(s) of book",
+)
+@click.option(
+    "-s",
+    "--size",
+    default="A5",
+    help=(
+        "Trim size of book: either a format name like 'A3', 'A4', 'A5', 'letter', "
+        "or 'legal', or dimensions in inches formatted '<width>x<height>', e.g., '7x10' "
+        "or '5.5x8.5' [default: 'A5']"
+    ),
+)
+@click.option(
+    "-c",
+    "--cover",
+    required=True,
+    type=click.Path(exists=True),
+    help="Path to a cover image file",
+)
+@click.option(
+    "-i",
+    "--interior",
+    required=True,
+    type=click.Path(exists=True),
+    help="Path to PDF of interior",
+)
+@click.option(
+    "-o",
+    "--outfile",
+    required=True,
+    type=click.Path(),
+    help="Path to generated output",
+)
 def run(title, author, size, cover, interior, outfile):
     buffer = get_cover_pages(size, cover)
     writer = pypdf.PdfWriter()
